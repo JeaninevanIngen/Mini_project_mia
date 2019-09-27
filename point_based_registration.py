3af6da26a310702fcc550608dcc8bc12b04e92a5
JeaninevanIngen <54934340+JeaninevanIngen@users.noreply.github.com>
2019-09-27 17:57:18 +0200

    Add files via upload

--- point_based_registration.py
+++ point_based_registration.py
@@ -21,6 +21,27 @@ import registration as reg
 import registration_util as util
 from IPython.display import display, clear_output
 
+def point_based_error(I_path,Im_path,T):
+    
+    #Select set of corresponding points using my_cpselect
+    X0, Xm0 = util.my_cpselect(I_path, Im_path)
+    
+    Xm = np.ones((3, Xm0.shape[1])) 
+    Xm[0,:] =Xm0[0,:]
+    Xm[1,:]=Xm0[1,:]
+    X = np.ones((3, X0.shape[1])) 
+    X[0,:] =X0[0,:]
+    X[1,:]=X0[1,:]
+    b1 = np.transpose(X[0,:])
+    b2 = np.transpose(X[1,:])
+    #Compute the affine transformation between the pair of images
+    _, Etestx = reg.ls_solve(T,b1)
+    _, Etesty = reg.ls_solve(T,b2)
+    Etest=np.array([[Etestx],[Etesty]])
+    
+    return Etest
+
+
 def point_based_registration_demo():
     # ---------------------------------
     # Append the following code in jupiter to run:
@@ -33,32 +54,42 @@ def point_based_registration_demo():
     
     # read the fixed and moving images, 2x T1 or T1&T2
     # change these in order to read different images
-    I_path = './data/image_data/1_1_t1.tif'
-    Im_path = './data/image_data/1_1_t1_d.tif'
+    I_path = '../data/image_data/1_1_t1.tif'
+    Im_path = '../data/image_data/1_1_t1d.tif'
     
     #Select set of corresponding points using my_cpselect
     X0, Xm0 = util.my_cpselect(I_path, Im_path)
     
     #Compute the affine transformation between the pair of images
-    #Xm=np.ones((Xm0.shape[0],3))
-    #wside=np.expand_dims(wside, axis=1)
     Xm = np.ones((3, Xm0.shape[1])) 
     Xm[0,:] =Xm0[0,:]
     Xm[1,:]=Xm0[1,:]
     X = np.ones((3, X0.shape[1])) 
     X[0,:] =X0[0,:]
     X[1,:]=X0[1,:]
-    T = reg.ls_affine(X, Xm)
+    T, Etrain = reg.ls_affine(X, Xm)
     
-
+    Etest=point_based_error(I_path,Im_path,T)
+    
+    print(Etest)
+    print(Etrain)
+    
     #read image
     Im=plt.imread(Im_path)
     
     #Apply the affine transformation to the moving image
     It, Xt = reg.image_transform(Im, T)
     
+    #plot figure
+    fig = plt.figure(figsize=(12,5))
+    fig, ax = plt.subplots()
+    
+    ax.set_title("Transformed image with error: Etrain={} Etest={}".format(Etrain,Etest))
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+
     plt.imshow(It)
     
-    return It, Xt
+
    
     
